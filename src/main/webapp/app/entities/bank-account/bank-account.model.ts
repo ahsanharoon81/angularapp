b4139f64cb85a949f344100b7c129d33dc7d7e0a
unknown <ahsan_haroon90@outlook.dk>
2021-03-15 10:38:45 +0100

    angular application

---
+++ src/main/webapp/app/entities/bank-account/bank-account.model.ts
@@ -0,0 +1,20 @@
+import { IUser } from 'app/entities/user/user.model';
+import { IOperation } from 'app/entities/operation/operation.model';
+
+export interface IBankAccount {
+  id?: number;
+  name?: string;
+  balance?: number;
+  user?: IUser | null;
+  operations?: IOperation[] | null;
+}
+
+export class BankAccount implements IBankAccount {
+  constructor(
+    public id?: number,
+    public name?: string,
+    public balance?: number,
+    public user?: IUser | null,
+    public operations?: IOperation[] | null
+  ) {}
+}
